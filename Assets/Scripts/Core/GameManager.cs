535366b7385e659d77b24709d48ec771846f6342
Antonio <mendao_22@hotmail.com>
2025-02-20 10:38:19 -0300

    Adjust GameManager

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,8 +1,41 @@
-using System.Collections;
+using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    //Dessa forma a classe ja vira um singleton
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .5f;
+    public Ease ease = Ease.OutBack;//Ajuste de animaçao deixar mais fluido
+
+    private GameObject _currentPlayer;
+
+    private void Start()
+    {
+        Init();
+        
+    }
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From();//efeito de escala
+    }
+
+
 }
