b340bfd4601f64a66a51fef1273bb418e57006ec
Antonio <mendao_22@hotmail.com>
2025-02-21 08:31:01 -0300

    Add Run Prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour
     //public Vector2 velocity;
     public Rigidbody2D myRigidBody;
     public float speed;
+    public float speedRun;
 
     public float forceJump = 5f;
 
     public Vector2 friction = new Vector2(-.1f, 0);
+
+    private float _currentSpeed;
 
     void Update()
     {
@@ -20,15 +23,24 @@ public class Player : MonoBehaviour
 
     public void HandleMoviment()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _currentSpeed = speedRun;
+        }
+        else
+        {
+            _currentSpeed = speed;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
-            myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(-_currentSpeed, myRigidBody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
-            myRigidBody.velocity = new Vector2(+speed, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(+_currentSpeed, myRigidBody.velocity.y);
         }
 
         if(myRigidBody.velocity.x > 0)//Adicionar uma fricçao para parar o player quando vai para direita
